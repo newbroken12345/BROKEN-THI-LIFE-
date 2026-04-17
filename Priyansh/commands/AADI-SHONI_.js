3ac5ae8d92825e2e7519f440f0e17972c82e18fa
newbroken12345 <newbotbroken@gmail.com>
2026-04-17 20:44:59 +0530

    Create AADI-SHONI_.js

---
+++ Priyansh/commands/AADI-SHONI_.js
@@ -0,0 +1,22 @@
+module.exports.config = {
+  name: "Shoni",
+  version: "1.0.0",
+  hasPermssion: 0,
+  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+  description: "Bot will rep ng tag admin or rep ng tagbot ",
+  commandCategory: "Other",
+  usages: "",
+  cooldowns: 1
+};
+module.exports.handleEvent = function({ api, event }) {
+  if (event.senderID !== "61566451472957") {
+    var aid = ["61566451472957"];
+    for (const id of aid) {
+    if ( Object.keys(event.mentions) == id) {
+      var msg = ["Abee Mere se baat Kar ..use mention kyu kar rhe ho😒", "Kya Hua Boss ko q Bula Rhe Ho?", "Wo Shayad Busy h😐🙄", "Tujhe sunai nhi deta mere admin ko tang na kr😒😒", "Mere boss ko tang naa karo", "Abee Bola na mere Admin ko bar bar mention mat karo ek bar me nahi smjh nahi ata", "Kya kaam hai mujhe bol", "Ek Baar me samjh ni ata , kyu mention kar rhe ho use" , "Mere Boss ko tang mt kiya kro smjhe😒"];
+      return api.sendMessage({body: msg[Math.floor(Math.random()*msg.length)]}, event.threadID, event.messageID);
+    }
+    }}
+};
+module.exports.run = async function({}) {
+        }
