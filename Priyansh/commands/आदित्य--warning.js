ea437124498bd3b9c8bf73c35b84ea4ac784e08b
newbroken12345 <newbotbroken@gmail.com>
2026-04-22 22:11:37 +0530

    Update and rename warning.js to आदित्य--warning.js

--- Priyansh/commands/आदित्य--warning.js
+++ Priyansh/commands/आदित्य--warning.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "warning",
 	version: "1.0.0",
 	hasPermssion: 2,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "— आदित्य हूं यार ː͢» 🩷🪽",
 	description: "Warning people!",
 	commandCategory: "system",
 	usages: "[ do/all]",
