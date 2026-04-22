f4123bb1f5ead3bb36d90051f49872e92adda427
newbroken12345 <newbotbroken@gmail.com>
2026-04-22 22:17:54 +0530

    Update and rename virgin.js to आदित्य--virgin.js

--- Priyansh/commands/आदित्य--virgin.js
+++ Priyansh/commands/आदित्य--virgin.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "virgin",
 	version: "1.0.0",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "— आदित्य हूं यार ː͢» 🩷🪽",
 	description: "Random picture of the most beautiful boy in Vietnam :))",
 	commandCategory: "Random-IMG",
 	usages: "virgin",
