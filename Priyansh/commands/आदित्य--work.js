aa66f650e27b1b8b8bc415173d85b12825caf3db
newbroken12345 <newbotbroken@gmail.com>
2026-04-22 22:04:41 +0530

    Update and rename work.js to आदित्य--work.js

--- Priyansh/commands/आदित्य--work.js
+++ Priyansh/commands/आदित्य--work.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "work",
 	version: "1.0.1",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "— आदित्य हूं यार ː͢» 🩷🪽",
 	description: "Work to earn money!",
 	commandCategory: "Entertainment",
     cooldowns: 5,
