2772339f4be3aeeabb0f57b67b2fdb88a395a34b
newbroken12345 <newbotbroken@gmail.com>
2026-04-22 22:21:14 +0530

    Update and rename uptime.js to आदित्य--uptime.js

--- Priyansh/commands/आदित्य--uptime.js
+++ Priyansh/commands/आदित्य--uptime.js
@@ -8,7 +8,7 @@ module.exports = {
     name: "uptime",
     version: "1.0.0",
     hasPermssion: 2,
-    credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+    credits: "— आदित्य हूं यार ː͢» 🩷🪽",
     description: "test",
     commandCategory: "box",
     usages: "test",
