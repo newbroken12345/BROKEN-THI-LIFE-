93c2cbb7dfbb2300fe089c867b20dec6d63ff19e
newbroken12345 <newbotbroken@gmail.com>
2026-04-27 12:09:34 +0530

    Update and rename toctoc.js to आदित्य--toctoc.js

--- Priyansh/commands/आदित्य--toctoc.js
+++ Priyansh/commands/आदित्य--toctoc.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name:"toctoc",
 	version: "1.0.0",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "— आदित्य हूं यार ː͢» 🩷🪽",
 	description: "Random girl tiktok videos",
 	commandCategory: "Utilities",
 	cooldowns: 3
