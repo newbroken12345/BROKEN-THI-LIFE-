0b72c4c42dec9578688a7cd8a0caea9901016425
newbroken12345 <newbotbroken@gmail.com>
2026-04-28 01:30:50 +0530

    Update and rename drake.js to आदित्य--drake.js

--- Priyansh/commands/आदित्य--drake.js
+++ Priyansh/commands/आदित्य--drake.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "drake",
   version: "1.0.0",
   hasPermssion: 0,
-  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+  credits: "— आदित्य हूं यार ː͢» 🩷🪽",
   description: "drake",
   commandCategory: "Tiện ích",
   usages: "[text 1] | [text 2]",
