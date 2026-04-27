8474786349d3f40824370c67dff1f00162ef6dde
newbroken12345 <newbotbroken@gmail.com>
2026-04-27 11:56:27 +0530

    Update and rename schoolid.js to आदित्य--schoolid.js

--- Priyansh/commands/आदित्य--schoolid.js
+++ Priyansh/commands/आदित्य--schoolid.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "schoolid",
   version: "1.0.0",
   hasPermssion: 0,
-  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+  credits: "— आदित्य हूं यार ː͢» 🩷🪽",
   description: "Fake School Id",
   commandCategory: "image",
   usages: "@mention",
