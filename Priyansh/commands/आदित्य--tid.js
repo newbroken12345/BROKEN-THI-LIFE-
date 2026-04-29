426e5f16a7308951df851696acb6720dd22d3b8c
newbroken12345 <newbotbroken@gmail.com>
2026-04-29 11:35:08 +0530

    Update and rename tid.js to आदित्य--tid.js

--- Priyansh/commands/आदित्य--tid.js
+++ Priyansh/commands/आदित्य--tid.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "tid",	
   version: "1.0.0", 
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "— आदित्य हूं यार ː͢» 🩷 🪽",
 	description: "Get box id", 
 	commandCategory: "group",
 	usages: "tid",
